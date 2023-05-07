780fe61cd9b589d08787c64358f6db84682b991a
Jeremy Myers <echelonical@gmail.com>
2023-05-07 13:20:41 -0500

    created routes to newly created pages/modules

--- pages/jeremy/jeremy-myers.js
+++ pages/jeremy/jeremy-myers.js
@@ -1,3 +1,10 @@
+import Link from "next/link";
+
 export default function FirstPost() {
-    return <h1>Welcome to Jeremy's Page...!</h1>;
+    return (
+        <>
+            <h1>Welcome to Jeremy's Page...!</h1>
+            <Link href="/">Back to Home Page</Link>
+        </>
+    )
 }
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,17 @@
+import Link from "next/link";
+
 export default function FirstPost() {
-    return <h1>First Post</h1>;
+    return (
+        <>
+            <h1>First Post</h1>
+            <h2>
+                <div>
+                    <Link href="/">Back to Home</Link>
+                </div>
+                <div>
+                    <Link href="/jeremy/jeremy-myers">To Jeremy's Link</Link>
+                </div>
+            </h2>
+        </>
+    )
 }
