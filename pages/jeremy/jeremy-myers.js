c21a968ebdd0252ca2ecd3c0a3a8aebc0950e159
Jeremy Myers <echelonical@gmail.com>
2023-05-07 13:57:25 -0500

    Used <Head> tag to dynamically change title of route endpoint.

--- pages/jeremy/jeremy-myers.js
+++ pages/jeremy/jeremy-myers.js
@@ -1,8 +1,12 @@
+import Head from "next/head";
 import Link from "next/link";
 
 export default function FirstPost() {
     return (
         <>
+            <Head>
+                <title>JayMy45</title>
+            </Head>
             <h1>Welcome to Jeremy's Page...!</h1>
             <Link href="/">Back to Home Page</Link>
         </>
--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,17 +1,24 @@
+import Head from "next/head";
 import Link from "next/link";
+import Layout from '../../components/layout';
 
 export default function FirstPost() {
     return (
         <>
-            <h1>First Post</h1>
-            <h2>
-                <div>
-                    <Link href="/">Back to Home</Link>
-                </div>
-                <div>
-                    <Link href="/jeremy/jeremy-myers">To Jeremy's Link</Link>
-                </div>
-            </h2>
+            <Layout>
+                <Head>
+                    <title>First Post</title>
+                </Head>
+                <h1>First Post</h1>
+                <h2>
+                    <div>
+                        <Link href="/">Back to Home</Link>
+                    </div>
+                    <div>
+                        <Link href="/jeremy/jeremy-myers">To Jeremy's Link</Link>
+                    </div>
+                </h2>
+            </Layout>
         </>
     )
 }
