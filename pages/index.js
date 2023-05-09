caed100a9ab79bc5f3ade9ceaac72f11401a806a
Jeremy Myers <echelonical@gmail.com>
2023-05-08 21:47:00 -0500

    styling updates

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
-import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
+import utilStyles from '../styles/utils.module.css';
+import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
 
 export async function getStaticProps() {
   const allPostsData = await getSortedPostsData();
